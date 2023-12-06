11cccf667c6f6d9ec213317caec7f29f4522d31f
renehellmich <renehellmich94@gmail.com>
2023-12-06 13:28:59 +0100

    Console-Level-1_1 completed

---
+++ Aufgaben/Console-Level-1_1/assets/js/main.js
@@ -0,0 +1,22 @@
+// 1. Beispiel
+
+let a = 12;
+let b = a * 3;
+console.log(b);
+
+console.log("Hello World");
+let firstName = "Anton";
+let age = 28;
+
+console.log(`${firstName}`);
+console.log(`${age}`);
+
+let job = "Trainer:in"
+console.log(job);
+
+let married = true;
+console.log(married);
+
+console.log(`${firstName} ist ${age} alt, von Beruf ${job} und ist verheiratet: ${married}`);
+
+document.querySelector('#output').innerHTML = b;
