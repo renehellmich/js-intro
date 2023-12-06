7c8147756eca1770960c28a5ce8e49ef0dd4d53e
renehellmich <renehellmich94@gmail.com>
2023-12-06 13:21:01 +0100

    Intro-Level-1_7 completed

---
+++ Aufgaben/Intro-Level-1_7/assets/js/main.js
@@ -0,0 +1,16 @@
+let helloWorld = "Hello World";
+
+const pi = Math.pi;
+
+let burjDubai = "828m";
+let fullName = "Jan Schmidt";
+let eifeltower = "324m";
+
+const camelCase = "camelCase";
+let kursStatus = true;
+
+helloWorld = "Hello_World";
+burjDubai = 828;
+
+document.write(burjDubai);
+console.log(burjDubai);
