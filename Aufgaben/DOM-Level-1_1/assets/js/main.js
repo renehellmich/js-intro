ff7849fec600284b0c7773d0f7d035079c3169bb
renehellmich <renehellmich94@gmail.com>
2023-12-06 13:45:41 +0100

    DOM-Level-1_1 completed

---
+++ Aufgaben/DOM-Level-1_1/assets/js/main.js
@@ -0,0 +1,13 @@
+document.write("<h1>Hello World!</h1>");
+document.write("<p>Have a nice day!</p>");
+
+let myText = "Hello again.";
+document.write(myText);
+
+let vorName;
+let nachName;
+
+vorName = "René";
+nachName = "Hellmich";
+
+document.write("<br>" + `Hi, ich bin ${vorName} ${nachName}.`);
