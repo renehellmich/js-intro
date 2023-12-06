0834250693f429f3db0a029d574a45ebb9cb14fa
renehellmich <renehellmich94@gmail.com>
2023-12-06 13:34:47 +0100

    Variablen-Level-1_5 completed

---
+++ Aufgaben/Variablen-Level-1_5/assets/js/main.js
@@ -0,0 +1,18 @@
+let carName = "BMW";
+console.log(carName);
+
+let x = 150;
+let y = 50;
+let z = x + y;
+
+console.log(z);
+
+let firstName;
+let lastName;
+let age;
+
+firstName = "John";
+lastName = "Doe";
+age = 35;
+
+console.log(`${firstName} ${lastName} ist ${age} Jahre alt.`);
