1b4f997ee61d99ae84ede76a65f2400b5a61f324
renehellmich <renehellmich94@gmail.com>
2023-12-07 13:56:32 +0100

    Functions-Level-3_1

---
+++ Aufgaben/Functions-Level-3_1/assets/js/main.js
@@ -0,0 +1,6 @@
+const headLine = document.querySelector("#headLine");
+
+function changeColor(color1, color2){
+    headLine.style.color = color1;
+    document.body.style.backgroundColor = color2;
+}
