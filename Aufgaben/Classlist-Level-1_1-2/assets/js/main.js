b4390db77ee48412d7e615df448889b60296ae05
renehellmich <renehellmich94@gmail.com>
2023-12-08 14:15:36 +0100

    classlist

---
+++ Aufgaben/Classlist-Level-1_1-2/assets/js/main.js
@@ -0,0 +1,10 @@
+function headlineRed() {
+    const headline = document.querySelector("#headline")
+    headline.classList.toggle("headlineRed");
+}
+
+function listBlue() {
+    const list = document.querySelector("#li");
+    console.log("listBlue");
+    list.classList.toggle("liBlue");
+}
