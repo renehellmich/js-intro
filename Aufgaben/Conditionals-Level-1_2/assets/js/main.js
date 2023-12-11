34598d1afb534fb7b30dee786795c149d167bf7d
renehellmich <renehellmich94@gmail.com>
2023-12-11 13:33:43 +0100

    Conditionals-Level-1_2 completed

---
+++ Aufgaben/Conditionals-Level-1_2/assets/js/main.js
@@ -0,0 +1,17 @@
+const output = document.querySelector("#output");
+
+function checkWeather() {
+    const number = document.querySelector("#number").value;
+
+    if (number <= 10 && number >= 8) {
+        output.innerHTML = "super";
+    } else if (number >= 6 && number <= 7) {
+        output.innerHTML = "gut";
+    } else if (number >= 3 && number <= 5) {
+        output.innerHTML = "okay";
+    } else if (number <= 2) {
+        output.innerHTML = "schlecht";
+    } else {
+        output.innerHTML = "Sie haben eine ungültige Zahl eingegeben!"
+    }
+}
