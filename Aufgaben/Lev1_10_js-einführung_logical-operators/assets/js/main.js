613c495762dd57408928ad8516f95d247d299094
renehellmich <renehellmich94@gmail.com>
2023-12-11 13:16:04 +0100

    Lev1_10 completed

---
+++ Aufgaben/Lev1_10_js-einführung_logical-operators/assets/js/main.js
@@ -0,0 +1,37 @@
+let x = 10;
+let y = 15;
+let z = 20;
+
+let a = "true";
+let b = "false";
+
+if ((x > z && x < y)) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+if (x != y) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+if (z < y || z > x) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+if (x === z || x != y) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+if (x >= 10 && y <= 10) {
+    console.log(a);
+} else {
+    console.log(b);
+}
+if (x * z < 100 || x * y > 100) {
+    console.log(a);
+} else {
+    console.log(b);
+}
