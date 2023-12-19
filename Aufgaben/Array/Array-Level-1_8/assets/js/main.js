591871c30e115073bfd5b2ac9c125285084eefce
renehellmich <renehellmich94@gmail.com>
2023-12-19 14:26:17 +0100

    Array-Level-1_8_10

---
+++ Aufgaben/Array/Array-Level-1_8/assets/js/main.js
@@ -0,0 +1,18 @@
+let country = ["Schweden", "Polen", "Litauen", "Norwegen", "Griechenland"]
+console.log(country);
+country = country.slice(2, 4);
+
+console.log(country);
+
+// * 1 - 10
+
+const text = "It's okay, Bender. I like cooking too. Oh, you're a dollar naughtier than most. My fellow Earthicans, as I have explained in my book 'Earth in the Balance', and the much more popular 'Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards."
+
+console.log(text);
+
+let split = text.split(" ");
+
+console.log(split);
+
+let splitLetters = text.split("");
+console.log(splitLetters);
