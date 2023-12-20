4a19807727c68b1e82309e831022bfedd5d404cd
renehellmich <renehellmich94@gmail.com>
2023-12-20 12:36:58 +0100

    Loops-Level-1_1-6

---
+++ Aufgaben/Loops/Loops-Level-1_1-6/assets/js/main.js
@@ -0,0 +1,40 @@
+for(let i = 1; i<=10; i++) {
+    console.log("Hello World ", i);
+}
+
+// * 1 - 2
+
+let numArr = []
+
+for (let i = 0; i <= 10; i++) {
+    numArr.push(i);
+}
+
+console.log(numArr);
+
+// * 1 - 3
+
+let counter = 0;
+
+while(counter < 10){
+    console.log("Hello World ", counter);
+    counter++;
+}
+
+// * 1 - 4
+
+const Arr = ["Georg", "Anass", "Elaine", "Hakan", "Eric", "Kim", "Sergio"];
+
+for(let i = 0; i < Arr.length; i++) {
+    console.log(Arr[i]);
+}
+
+// * 1 - 6
+
+let letArray = []
+
+for(let i = 1; i <= 100; i++) {
+    letArray.push(`image\_${i}.jpg`);
+}
+
+console.log(letArray);
