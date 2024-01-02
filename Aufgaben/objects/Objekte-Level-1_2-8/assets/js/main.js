7a8b67406648b4abce75e75502a9b3872f522a64
renehellmich <renehellmich94@gmail.com>
2024-01-02 13:19:42 +0100

    Object 1 - 8

--- Aufgaben/objects/Objekte-Level-1_2-8/assets/js/main.js
+++ Aufgaben/objects/Objekte-Level-1_2-8/assets/js/main.js
@@ -129,3 +129,43 @@ myMusic
 
     })
 
+// 1 - 7
+console.log("=== Objects 1 - 7 ===");
+
+let studentData = [
+    {
+        name: "Alex",
+        age: 23,
+        coop: false,
+        address: {
+            street: "Don Valley Business Park",
+            city: "Toronto",
+            postalCode: "ONM3C3E5"
+        },
+        emails: ["alex69@gmail.com", "alex123@yahoo.com"]
+    },
+    {
+        name: "Sandra",
+        age: 22,
+        coop: true,
+        address: {
+            street: "34 Lawrence Ave",
+            city: "Toronto",
+            postalCode: "ONM3C0E5"
+        },
+        emails: ["sandra@gmail.com", "sandra@yahoo.com"]
+    }
+];
+
+console.log(studentData);
+const outputData = () => {
+    studentData
+        .forEach((element) => {
+            console.log(element.name);
+            console.log(element.coop);
+            console.log(element.emails);
+        })
+    console.log(studentData.map(e => e.address.city));
+}
+
+outputData();
