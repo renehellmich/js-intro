0baad268917d88f675a7fee5805633ff76a58cef
renehellmich <renehellmich94@gmail.com>
2024-01-03 11:30:25 +0100

    Date 1 - 4

--- Aufgaben/Date/Date-Level-1_1,4,5,7/assets/js/main.js
+++ Aufgaben/Date/Date-Level-1_1,4,5,7/assets/js/main.js
@@ -9,3 +9,16 @@ div.innerHTML = `${date1} = new Date("September 2, 2019 09:00:00")`
 div.innerHTML += "<br>" + `${date2} = new Date(0)`
 div.innerHTML += "<br>" + `${date3} = new Date(31446908800)`
 div.innerHTML += "<br>" + `${date4} = new Date(86400000)`
+
+// 1 - 4
+console.log("=== Date 1 - 4 ===");
+
+const tageImMonat = (monat, jahr) => {
+    const date = new Date(jahr, monat)
+    console.log(date.getUTCDate());
+}
+
+tageImMonat(1, 2016)
+tageImMonat(2, 2016)
+tageImMonat(2, 2017)
+tageImMonat(12, 2017)
