07747b0c70306b0dcc848051235e5de061a90015
renehellmich <renehellmich94@gmail.com>
2024-01-04 12:56:27 +0100

    completed

---
+++ Aufgaben/BOM/Bonus/assets/js/main.js
@@ -0,0 +1,68 @@
+const cat = document.querySelector("#cat")
+console.log(cat.clientWidth);
+
+const winWidth = window.innerWidth
+console.log(winWidth, cat.clientWidth);
+
+
+let posAbs = 0
+let speed = 500
+let direction = "+"
+let catInterval
+
+const rightBound = winWidth - cat.clientWidth + posAbs
+console.log(rightBound);
+
+const goCat = () => {
+    console.log("speed", speed);
+    if (posAbs >= 0 && posAbs <= rightBound && direction === "+") {
+        direction = "+"
+        posAbs += Number(`${direction}24`)
+        cat.style.left = `${posAbs}px`
+    } else if (posAbs < 0 && direction === "-") {
+        changeDirection(0)
+        direction = "+"
+        posAbs += Number(`${direction}24`)
+        cat.style.left = `${posAbs}px`
+    } else {
+        changeDirection(180)
+        direction = "-"
+        posAbs += Number(`${direction}24`)
+        cat.style.left = `${posAbs}px`
+    }
+}
+
+const changeDirection = (deg) => {
+    cat.style.transform = `rotateY(${deg}deg)`
+}
+
+
+// Functionsaufrufe
+
+function catWalk() {
+    // goCat()
+    catInterval = setInterval(goCat, speed)
+}
+
+function pause() {
+    clearInterval(catInterval)
+}
+
+function turn() {
+    switch(direction) {
+        case "+":
+            changeDirection(180)
+            direction = "-"
+            break;
+        case "-":
+            changeDirection(0)
+            direction = "+"
+            break;
+    }
+}
+
+function catSpeed() {
+    speed = Number(prompt("Bitte geben Sie eine Geschwindigkeit ein:", ))
+    pause()
+    catWalk()
+}
